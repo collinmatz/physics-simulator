9bd8f5414a3181d4033154b07fb94d93c34f88b8
collinmatz <63628443+collinmatz@users.noreply.github.com>
2022-11-28 17:24:52 -0600

    added -help option

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 
 int main(int argc, char **argv) {
     // parse command line arguments
-    const int MAX_ARGS = 10;
-    if ((argc - 1) > MAX_ARGS) {
+    const int MAX_ARGS = -1;
+    if (MAX_ARGS > 0 && (argc - 1) > MAX_ARGS) {
         std::cout << "Error. Number of arguments exceeds maximum number alloted\n";
         return -1;
     }
@@ -18,13 +18,15 @@ int main(int argc, char **argv) {
     float dt = 1.0f;
     float gravity = -9.81f;
     for (int i = 0; i < argc; ++i) {
+        if (std::strcmp(argv[i], "-help") == 0) {
+            std::cout << " \
+            -m : specifies that each Physics World step occurs manually. Defaults to automatic\n \
+            -t {time_step} : the amount of time that each step should consist of. Defaults to 1.0\n \
+            -g {gravity} : the acceleration due to gravity. Defaults to -9.81\n";
+        }
+
         if (std::strcmp(argv[i], "-m") == 0) {
-            if (i + 1 > argc) {
-                std::cout << "Expected argument after -m" << '\n';
-                return -2;
-            }
-            if (std::strcmp(argv[i+1], "manualStep") == 0)
-                mode = 1;
+            mode = 1;
         }
         if (std::strcmp(argv[i], "-t") == 0) {
             if (i + 1 > argc) {
@@ -45,8 +47,8 @@ int main(int argc, char **argv) {
     PhysicsWorld pw(dt, gravity);
 
     Vector3* pos = ComponentBuilder::vector3(0.0f, 0.0f, 0.0f);
-    Vector3* velo  = ComponentBuilder::vector3(1.0f, -1.0f, 2.5f);
-    Vector3* f = ComponentBuilder::vector3(1.0f, 2.0f, 3.0f);
+    Vector3* velo  = ComponentBuilder::vector3(0.0f, 0.0f, 0.0f);
+    Vector3* f = ComponentBuilder::vector3(0.0f, 0.0f, 0.0f);
     Object* obj = ComponentBuilder::object(pos, velo, f, 1.0f);
 
     pw.addObject(obj);
