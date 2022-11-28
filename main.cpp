916174fe4db9a7eb99bcd27bdbf65881776d4b84
collinmatz <63628443+collinmatz@users.noreply.github.com>
2022-11-27 19:58:22 -0600

    added command line options

--- main.cpp
+++ main.cpp
@@ -8,19 +8,41 @@
 
 int main(int argc, char **argv) {
     // parse command line arguments
-    const int MAX_ARGS = 1;
+    const int MAX_ARGS = 10;
     if ((argc - 1) > MAX_ARGS) {
         std::cout << "Error. Number of arguments exceeds maximum number alloted\n";
         return -1;
     }
 
-    bool manualStep = false;
+    int mode = 0;
+    float dt = 1.0f;
+    float gravity = -9.81f;
     for (int i = 0; i < argc; ++i) {
-        if (strcmp(argv[i], "manualStep") == 0)
-            manualStep = true;
+        if (std::strcmp(argv[i], "-m") == 0) {
+            if (i + 1 > argc) {
+                std::cout << "Expected argument after -m" << '\n';
+                return -2;
+            }
+            if (std::strcmp(argv[i+1], "manualStep") == 0)
+                mode = 1;
+        }
+        if (std::strcmp(argv[i], "-t") == 0) {
+            if (i + 1 > argc) {
+                std::cout << "Expected argument after -t" << '\n';
+                return -2;
+            }
+            dt = std::stof(argv[i+1]);
+        }
+        if (std::strcmp(argv[i], "-g") == 0) {
+            if (i + 1 > argc) {
+                std::cout << "Expected argument after -g" << '\n';
+                return -2;
+            }
+            gravity = std::stof(argv[i+1]);
+        }
     }
 
-    PhysicsWorld pw;
+    PhysicsWorld pw(dt, gravity);
 
     Vector3* pos = ComponentBuilder::vector3(0.0f, 0.0f, 0.0f);
     Vector3* velo  = ComponentBuilder::vector3(1.0f, -1.0f, 2.5f);
@@ -30,7 +52,7 @@ int main(int argc, char **argv) {
     pw.addObject(obj);
 
     // main loop
-    if (manualStep) {
+    if (mode == 1) {
         do {
             pw.printObjectInfo();
             pw.step(1.0f);
--- physicsWorld.hpp
+++ physicsWorld.hpp
@@ -5,8 +5,10 @@
 class PhysicsWorld {
     private:
         std::vector<Object*> objects;
-        const float gravity = -9.81f;
+        float gravity;
+        float dt;
     public:
+        PhysicsWorld(float dt, float gravity);
         void addObject(Object* obj);
         void removeObject(Object* obj);
         void step(float dt);
